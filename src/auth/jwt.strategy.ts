623842c415de0006710ddb78b8bccc1bf6a4f14a
gustavo14maciel <gustavo14maciel@gmail.com>
2024-02-04 12:31:16 -0300

    style: Format whitespaces

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,24 +8,24 @@ dotenv.config()
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
-        super({
-            secretOrKeyProvider: passportJwtSecret({
-            cache: true,
-            rateLimit: true,
-            jwksRequestsPerMinute: 5,
-            jwksUri: `${process.env.JWT_ISSUER}.well-known/jwks.json`,
-            }),
+  constructor() {
+    super({
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${process.env.JWT_ISSUER}.well-known/jwks.json`,
+      }),
 
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            audience: process.env.JWT_AUDIENCE,
-            issuer: `${process.env.JWT_ISSUER}`,
-            algorithms: ['RS256'],
-        });
-    }
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      audience: process.env.JWT_AUDIENCE,
+      issuer: `${process.env.JWT_ISSUER}`,
+      algorithms: ['RS256'],
+    });
+  }
 
-    validate(payload: unknown): unknown {
-        console.log(payload)
-        return payload;
-    }
+  validate(payload: unknown): unknown {
+    console.log(payload)
+    return payload;
+  }
 }
